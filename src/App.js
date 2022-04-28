e2844a63ade6fead61709eed7e8112577556158a
Chris M <cbmorrison15@gmail.com>
2022-04-28 13:32:08 -0400

    Updated fetch

--- src/App.js
+++ src/App.js
@@ -1,50 +1,28 @@
-//Initial code from nucamp//
-//import logo from "./logo.svg";
-import "./App.css";
-import React, { Component } from "react";
-import { Navbar, NavbarBrand } from "reactstrap";
-import Directory from "./components/DirectoryComponent";
-import { CAMPSITES } from './shared/campsites.js';
 
-//Initial code for step 6//
-/*function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}*/
+import React, { Component } from "react";
+import Main from './components/MainComponent';
+import { BrowserRouter } from 'react-router-dom';
+import './App.css';
+import { Provider } from 'react-redux';
+import { ConfigureStore } from './redux/configureStore';
+
+
+
+const store = ConfigureStore();
+
+
+
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      campsites: CAMPSITES
-    };
-  };
   render() {
     return (
-      <div className="App">
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">NuCamp</NavbarBrand>
+      <Provider store={store}>
+        <BrowserRouter>
+          <div className="App">
+            <Main />
           </div>
-        </Navbar>
-        <Directory campsites={this.state.campsites} />
-      </div>
+        </BrowserRouter>
+      </Provider>
     );
-  }
+  };
 }
 export default App;
---
+++ src/components/CampsiteInfoComponent.js
@@ -0,0 +1,198 @@
+import React, { Component } from 'react';
+import {
+  Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem,
+  Button, Modal, ModalHeader, ModalBody, Label
+} from 'reactstrap';
+import { Link } from 'react-router-dom';
+import { Control, LocalForm, Errors } from 'react-redux-form';
+import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
+
+//Week 4 Task 3: Validation
+const required = val => val && val.length;
+const maxLength = len => val => !val || (val.length <= len);
+const minLength = len => val => val && (val.length >= len);
+
+//Task 2: Moved card code from directory to here
+function RenderCampsite({ campsite }) {
+  return (
+    <div className='col-md-5 m-1'>
+      <Card>
+        <CardImg top src={baseUrl + campsite.image} alt={campsite.name} />
+        <CardBody>
+          <CardText>{campsite.description}</CardText>
+        </CardBody>
+      </Card>
+    </div>
+  );
+}
+
+//Task 3: A description will appear on the right side of the description card once rendered
+//Week 4 Task 1: Rendering CommentForm to display the button
+function RenderComments({ comments, addComment, campsiteId }) {
+  if (comments) {
+    return (
+      <div className='col-md-5 m-1'>
+        <h4>Comments</h4>
+        {comments.map(comment => {
+          return (
+            <div key={comment.id}>
+              <p>
+                {comment.text}<br />
+                -- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))}
+              </p>
+            </div>
+          );
+        })}
+        <CommentForm campsiteId={campsiteId} addComment={addComment} />
+      </div>
+    );
+  }
+  return <div />
+}
+
+//Task 1: Checking to see if campsite is evaluated as truthy/falsy (logic)
+//If statement will return empty <div> w/bootrap row if truthy and falsy will return empty <div>
+
+//Task 2: Calling renderCampsite: The campsite will appear below the 4 cards with an img+description
+function CampsiteInfo(props) {
+  if (props.isLoading) {
+    return (
+      <div className="container">
+        <div className="row">
+          <Loading />
+        </div>
+      </div>
+    );
+  }
+  if (props.errMess) {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <h4>{props.errMess}</h4>
+          </div>
+        </div>
+      </div>
+    );
+  }
+  if (props.campsite) {
+    return (
+      <div className='container'>
+        <div className='row'>
+          <div className="col">
+            <Breadcrumb>
+              <BreadcrumbItem><Link to='/directory'>Directory</Link></BreadcrumbItem>
+              <BreadcrumbItem active>{props.campsite.name}</BreadcrumbItem>
+            </Breadcrumb>
+            <h2>{props.campsite.name}</h2>
+            <hr />
+          </div>
+        </div>
+        <div className='row'>
+          <RenderCampsite campsite={props.campsite} />
+          <RenderComments
+            comments={props.comments}
+            addComment={props.addComment}
+            campsiteId={props.campsite.id}
+          />
+        </div>
+      </div>
+    );
+  }
+  return <div />
+}
+
+//Week 4 Task 1: Created a CommentForm class comp. with a button
+class CommentForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isModalOpen: false
+    };
+    this.toggleModal = this.toggleModal.bind(this);
+
+  }
+
+  toggleModal() {
+    this.setState({
+      isModalOpen: !this.state.isModalOpen
+    });
+  }
+
+  handleSubmit(values) {
+    this.toggleModal();
+    this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
+  }
+
+  render() {
+    return (
+      <>
+        <Button outline onClick={this.toggleModal}>
+          <i className="fa fa-pencil fa-lg">Submit Comment</i>
+        </Button>
+        {/* Week 4 Task 2: Setting up a modal with author, text and textarea */}
+        <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+          <ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader>
+          <ModalBody>
+            <LocalForm onSubmit={values => this.handleSubmit(values)}>
+              <div className='form-group'>
+                <Label htmlFor="rating">Rating</Label>
+                <Control.select model='.rating' id='rating' name='rating'
+                  className='form-control'>
+                  <option>1</option>
+                  <option>2</option>
+                  <option>3</option>
+                  <option>4</option>
+                  <option>5</option>
+                </Control.select>
+              </div>
+              <div className='form-group'>
+                <Label htmlFor="author">Your Name</Label>
+                <Control.text model='.author' id='author' name='author'
+                  placeholder='Your Name'
+                  className='form-control'
+                  validators={{
+                    required,
+                    minLength: minLength(2),
+                    maxLength: maxLength(15)
+                  }}
+                />
+                <Errors
+                  className='text-danger'
+                  model='.author'
+                  show='touched'
+                  component='div'
+                  messages={{
+                    required: 'Required',
+                    minLength: 'Must be at least 2 characters',
+                    maxLength: 'Must be 15 characters or less'
+                  }}
+                />
+              </div>
+              <div className='form-group'>
+                <Label htmlFor="text">Comment</Label>
+                <Control.textarea model='.text' id='text' name='text'
+                  className='form-control' rows='6' />
+              </div>
+              <Button type="submit" value="submit" color="primary">Submit</Button>
+            </LocalForm>
+          </ModalBody>
+        </Modal>
+      </>
+    );
+  }
+}
+
+export default CampsiteInfo;
+
+
+
+
+
+
+
+
+
+
